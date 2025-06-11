c443b2f310dedf795cb4e5bcf6754b51de4ebc47
Miguel Garcia <miguel_garcia@trimble.com>
2025-06-11 13:58:16 -0700

    test1-gh-actions

---
+++ terraform/develop/main.tf
@@ -0,0 +1,16 @@
+# main.tf
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "example.txt"
+  content  = "This file was created by Terraform!"
+}
